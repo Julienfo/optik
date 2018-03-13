916601f928ad60678de04fadeb13835fc25f2cf8
delainebenoit <delaine.ben6223@gmail.com>
2018-03-13 22:45:07 +0100

    amélioration listing

--- resources/views/liste.blade.php
+++ resources/views/liste.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1>Listing du materiel</h1>
 
+    <li> NOM | REFERENCE | TYPE | QUALITE </li>
     @foreach($materiel as $m)
 
         <li>{{$m->nom_mat}} | {{$m->reference_mat}} | {{$m->type_mat}} | {{$m->qualite}}</li>
